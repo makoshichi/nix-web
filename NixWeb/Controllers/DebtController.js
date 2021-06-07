2b7bbfddd5c334540b0ea4292b9c5696700e19ae
Eduardo Hoeller <eduardo.hoeller@outlook.com>
2021-06-07 06:47:42 -0300

    Implementação front-end em AngularJS

---
+++ NixWeb/Controllers/DebtController.js
@@ -0,0 +1,31 @@
+﻿app.controller('DebtController', function ($scope, $http) {
+    $scope.filter = {};
+    $scope.errorMessage = {};
+    $scope.GetStatement = function () {
+        $http.post('https://localhost:44326/api/Debt/GetStatement', $scope.filter)
+            .then(function (response) {
+                $scope.data = response.data;
+                $scope.errorMessage = '';
+            },
+                function (err) {
+                    $scope.errorMessage = 'Falha na requisição: \n' 
+
+                    if (typeof err.data.errors !== 'undefined') {
+                        if (typeof err.data.errors.InitialDate !== 'undefined') {
+                            for (i = 0; i < err.data.errors.InitialDate.length; i++){
+                                $scope.errorMessage += err.data.errors.InitialDate[i] + "\r\n";
+                            }
+                        }
+                        if (typeof err.data.errors.FinalDate !== 'undefined') {
+                            for (i = 0; i < err.data.errors.FinalDate.length; i++) {
+                                $scope.errorMessage += err.data.errors.FinalDate[i] + "\r\n";
+                            }
+                        }
+                    }
+                    else
+                        $scope.errorMessage += err.data;
+
+                    console.log(err);
+                });
+    }
+});
